7a225cb6945cee4a1f08868785b0bf5c3ed2b45f
Alejandro <Alejandro.Gonzalez47@alu.uclm.es>
2025-02-23 20:14:02 +0100

    laberinto modificado a 23 febrero

--- juego.py
+++ juego.py
@@ -2,18 +2,29 @@ class ElementoMapa:
     def __init__(self):
         pass
 
-class Habitacion(ElementoMapa):
-    def __init__(self, num):
-        super().__init__()
-        self.num = num
-        self.norte = None
-        self.sur = None
-        self.este = None
-        self.oeste = None
+    def entrar(self):
+        raise NotImplementedError("Método debe ser implementado en la subclase")
+
 
 class Pared(ElementoMapa):
     def __init__(self):
         super().__init__()
+
+    def entrar(self):
+        print("Te has chocado con la pared")
+
+
+class ParedBomba(Pared):
+    def __init__(self, activa=False):
+        super().__init__()
+        self.activa = activa
+
+    def entrar(self):
+        if self.activa:
+            print("¡Bomba explotó!")
+        else:
+            print("Te has chocado con la pared bomba")
+
 
 class Puerta(ElementoMapa):
     def __init__(self, lado1, lado2):
@@ -28,6 +39,26 @@ class Puerta(ElementoMapa):
     def cerrar(self):
         self.abierta = False
 
+    def entrar(self):
+        if self.abierta:
+            print("La puerta está abierta")
+        else:
+            print("La puerta está cerrada")
+
+
+class Habitacion(ElementoMapa):
+    def __init__(self, num):
+        super().__init__()
+        self.num = num
+        self.norte = None
+        self.sur = None
+        self.este = None
+        self.oeste = None
+
+    def entrar(self):
+        print(f"Estás en la habitación {self.num}")
+
+
 class Laberinto(ElementoMapa):
     def __init__(self):
         super().__init__()
@@ -36,18 +67,89 @@ class Laberinto(ElementoMapa):
     def agregar_habitacion(self, habitacion):
         self.habitaciones.append(habitacion)
 
+
+class Modo:
+    def actua(self, bicho):
+        self.camina(bicho)
+
+    def camina(self, bicho):
+        raise NotImplementedError("Método debe ser implementado en la subclase")
+
+    def es_agresivo(self):
+        return False
+
+
+class Perezoso(Modo):
+    def es_agresivo(self):
+        return False
+
+
+class Agresivo(Modo):
+    def es_agresivo(self):
+        return True
+
+
+class Bicho:
+    def __init__(self, vidas=5, poder=5, modo=None, posicion=None):
+        self.vidas = vidas
+        self.poder = poder
+        self.modo = modo if modo else Perezoso()
+        self.posicion = posicion
+
+    def actua(self):
+        self.modo.actua(self)
+
+    def es_agresivo(self):
+        return self.modo.es_agresivo()
+
+
+class Bomba:
+    def __init__(self, activa=False):
+        self.activa = activa
+
+    def entrar(self):
+        if self.activa:
+            print("¡Bomba explotó!")
+        else:
+            print("No pasó nada")
+
+
+class Creator:
+    def cambiar_modo_agresivo(self, bicho):
+        bicho.modo = Agresivo()
+        bicho.poder = 10
+
+    def fabricar_bicho_agresivo(self):
+        return Bicho(modo=Agresivo(), vidas=5, poder=5)
+
+
+class CreatorB(Creator):
+    def fabricar_pared(self):
+        return ParedBomba()
+
+
+class Decorator(ElementoMapa):
+    def __init__(self, elemento):
+        self.elemento = elemento
+
+    def entrar(self):
+        self.elemento.entrar()
+
+
 class Juego:
     def __init__(self):
         self.laberinto = Laberinto()
+        self.bichos = []
+
+    def agregar_bicho(self, bicho):
+        self.bichos.append(bicho)
 
     def crear_laberinto(self):
-        # Crear habitaciones
         hab1 = Habitacion(1)
         hab2 = Habitacion(2)
         hab3 = Habitacion(3)
         hab4 = Habitacion(4)
 
-        # Configurar conexiones entre habitaciones
         puerta1 = Puerta(hab1, hab2)
         puerta2 = Puerta(hab2, hab3)
         puerta3 = Puerta(hab3, hab4)
@@ -59,7 +161,6 @@ class Juego:
         hab3.este = puerta3
         hab4.oeste = puerta3
 
-        # Agregar habitaciones al laberinto
         self.laberinto.agregar_habitacion(hab1)
         self.laberinto.agregar_habitacion(hab2)
         self.laberinto.agregar_habitacion(hab3)
@@ -68,7 +169,7 @@ class Juego:
     def jugar(self):
         self.crear_laberinto()
         print("¡Bienvenido al juego del laberinto!")
-        # Aquí puedes agregar la lógica del juego
+
 
 # Ejemplo de uso
 juego = Juego()
